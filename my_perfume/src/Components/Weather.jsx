d01c714edbfe45b14022eab7fbafc0fe40e90ba4
parkkingcar <parkkingcar@naver.com>
2022-05-08 19:42:23 +0900

    feat retry hotwo

--- my_perfume/src/Components/Weather.jsx
+++ my_perfume/src/Components/Weather.jsx
@@ -46,11 +46,8 @@ return(
     <div>
         
         temp:{temp}
-<<<<<<< HEAD
 
-=======
         <br/><br/>
->>>>>>> dd5ace9e48ea53b249aa846e6d15ee2fb3d317d1
         weather:{weather}
     </div>
 )
